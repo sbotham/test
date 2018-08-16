a56af14c7c5d8f587efd9c09c17f2f1ab21e56bb
Steve Botham <sbotham@arccorp.com>
2018-08-16 14:34:54 -0400

    Add startpoint

---
+++ src/main/java/StartPoint.java
@@ -0,0 +1,23 @@
+package com.arccorp;
+
+
+
+
+public class StartPoint {
+	
+
+	
+	public static void main(String[] args) {
+
+		restClient();
+	}
+	
+		
+	public static void restClient() {
+		String mName = "restClient";
+        System.out.println(mName+" Starts");
+		
+	}
+	
+	
+}
